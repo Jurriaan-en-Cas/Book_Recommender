6e957f5fce8ecb3357cb76f554ab59350fa4f074
DevDonut <41371020+DevDonut@users.noreply.github.com>
2023-01-08 21:49:21 +0100

    Add basic database operations

--- database/database_handler.py
+++ database/database_handler.py
@@ -3,12 +3,45 @@ import sqlite3
 CONNECTION_STRING = "BR_DB"
 
 
-def execute_query_without_result(query):
-    connection = create_connection()
-    cursor = connection.cursor()
-    cursor.execute(query)
-    connection.commit()
-    connection.close()
+def get_all_recommended_books_for_user(user_name):
+    user = get_user(user_name)
+    query = "SELECT * FROM (SELECT * FROM Recommendation WHERE User_id = {}) AS Recommendation INNER JOIN " \
+            "Book ON Book.Id = Recommendation.Book_id".format(user[0])
+    print(query)
+    result = execute_query_with_result(query)
+    return result
+
+
+def add_recommended_book(user_name, book_name):
+    book = get_book(book_name)
+    user = get_user(user_name)
+    print(user[0])
+    query = "INSERT INTO Recommendation (User_id, Book_id) VALUES ({}, {})".format(user[0], book[0])
+    if book is None:
+        create_book(book_name, 0)
+    if user is None:
+        create_user(user_name)
+    execute_query_without_result(query)
+
+
+def get_user(user_name):
+    query = "SELECT * FROM User WHERE Name = '{}'".format(user_name)
+    return execute_query_with_result(query)[0]
+
+
+def create_user(user_name):
+    query = "INSERT INTO User (Name) VALUES ('{}')".format(user_name)
+    execute_query_without_result(query)
+
+
+def get_book(book_name):
+    query = "SELECT * FROM Book WHERE Name = '{}'".format(book_name)
+    return execute_query_with_result(query)[0]
+
+
+def create_book(book_name, genre_id):
+    query = "INSERT INTO Book (Name, Genre_id) VALUES('{}', {})".format(book_name, genre_id)
+    execute_query_without_result(query)
 
 
 def execute_query_with_result(query):
@@ -17,6 +50,14 @@ def execute_query_with_result(query):
     result = cursor.execute(query).fetchall()
     connection.close()
     return result
+
+
+def execute_query_without_result(query):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
 
 
 def create_connection():
@@ -30,8 +71,7 @@ def create_db():
 
     tables_to_create.append("CREATE TABLE IF NOT EXISTS User ("
                             "Id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                            "Name VARCHAR(64) NOT NULL,"
-                            "Password VARCHAR(64) NOT NULL)")
+                            "Name VARCHAR(64) NOT NULL)")
 
     tables_to_create.append("CREATE TABLE IF NOT EXISTS User_Book ("
                             "User_id INTEGER NOT NULL,"
@@ -50,6 +90,10 @@ def create_db():
     tables_to_create.append("CREATE TABLE IF NOT EXISTS Genre ("
                             "Id INTEGER PRIMARY KEY AUTOINCREMENT,"
                             "Name VARCHAR(64) NOT NULL)")
+
+    tables_to_create.append("CREATE TABLE IF NOT EXISTS Recommendation ("
+                            "User_id INTEGER NOT NULL,"
+                            "Book_id INTEGER NOT NULL)")
 
     for table_query in tables_to_create:
         cur.execute(table_query)
